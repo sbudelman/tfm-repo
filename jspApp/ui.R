15fec5091630098f7269cbf6be92c02ac7139c32
Samuel Udelman <samueludelman@gmail.com>
2019-02-26 23:32:37 +0100

    Add home page and update UI

--- jspApp/ui.R
+++ jspApp/ui.R
@@ -5,26 +5,52 @@ library(timevis)
 # Define UI for data upload app ----
 ui <- navbarPage("Shiny JSP",
                  
-  tabPanel("Home"),
+  tabPanel("Home",
+           
+    sidebarLayout(
+      position = "right",
+      sidebarPanel(
+        
+        h2(style = "margin-bottom: 2em",
+          "Get started"),
+        
+        textInput("planID", label = "Enter Plan ID", placeholder = "Enter Plan ID..."),
+        
+        # Input: Select a file ----
+        fileInput("file1", div("Upload data file",tags$a(href="", "(template here)")),
+                  placeholder = "Upload .XLSX file",
+                  multiple = FALSE,
+                  accept = c(".xlsx")),
+        
+        uiOutput('ui.plan'),
+        
+        uiOutput('ui.solve'),
+        
+        hr(),
+        
+        p("Dummy text"),
+        
+        width = 4),
+      
+      mainPanel(
+        
+        includeHTML("text/home.html"),
+        width = 8
+      )
+    )
+           
+           
+           ),
   
   tabPanel("Data",
   
     # Sidebar layout with input and output definitions ----
     sidebarLayout(
       
+      position = "right",
+      
       # Sidebar panel for inputs ----
       sidebarPanel(
-        
-  
-        
-        # Input: Select a file ----
-        fileInput("file1", "Upload .XLSX File",
-                  multiple = FALSE,
-                  accept = c(".xlsx")),
-        
-        uiOutput('ui.plan'),
-        
-        uiOutput('ui.solve')
         
       ),
       
@@ -59,19 +85,23 @@ ui <- navbarPage("Shiny JSP",
   
   tabPanel("Plan",
            
-           fluidRow(
+           sidebarLayout(
              
-             column(8,
+             position = "right",
+             
+             sidebarPanel(
+               p("other dummy text")
+             ),
+             
+             mainPanel(
                     tabsetPanel(type = "tabs",
                                 tabPanel("Job's schedule", timevisOutput("jobsVis")),
                                 tabPanel("Machines utilisation", timevisOutput("machinesVis"))
                     )
                     
-             ),
-             
-             column(4,
-                    p("other dummy text")
              )
+             
+             
            )
   )
 
--- jspApp/utilityFunctions.R
+++ jspApp/utilityFunctions.R
@@ -211,24 +211,149 @@ solveMod <- function(excelFile){
              `Task Starting Time` + as.difftime(tim = as.numeric(`Task Runtime`),
                                                 format = "%M", units = "mins"))
   
-  # Generate data frame for a Job-based timeline
-  jobsView <- data.frame("start" = startValues$`Task Starting Time`,
-                         "end" = startValues$`Task Ending Time`,
-                         "content" = startValues$`Task Name`,
-                         "group" = startValues$`Job ID`)
   
-  jobsViewGroups <- data.frame(id = unique(startValues$`Job ID`), 
-                               content = c(sprintf(paste("Job %s"),seq(1:n_distinct(startValues$`Job ID`))))
+  
+  # Include shifts
+  shift <- matrix(c('12:00', '15:00',
+                    '17:00', '19:00',
+                    '19:00', '21:00' ),
+                  nrow = 3, ncol = 2, byrow = TRUE)
+  
+  # Function to generate shift blocks per day
+  
+  shiftBlocks <- function (day, shifts){
+    
+    shiftBlocks <- c()
+    
+    for(row in 1:nrow(shifts)){
+      
+      if(shifts[row,1] > shifts[row,2]){
+        
+        shiftBlocks <- rbind(shiftBlocks, c(format(as.POSIXct(
+          paste0(day, ' ', shifts[row,1], ':00')), 
+          '%Y-%m-%d %H:%M:%S'),
+          format(as.POSIXct(
+            paste0(as.Date(day)+1, ' ', shifts[row,2], ':00')), 
+            '%Y-%m-%d %H:%M:%S')
+        ))
+        
+      } else {
+        
+        shiftBlocks <- rbind(shiftBlocks, c(format(as.POSIXct(
+          paste0(day, ' ', shifts[row,1], ':00')), '%Y-%m-%d %H:%M:%S'),
+          format(as.POSIXct(
+            paste0(day, ' ', shifts[row,2], ':00')), 
+            '%Y-%m-%d %H:%M:%S')
+        ))
+      }
+      
+      
+    }
+    
+    return(shiftBlocks)
+    
+  }
+  
+  # Function to implement shifts on results
+  shiftedTasks <- function(taskTimes, shifts){
+    
+    day <- format(min(taskTimes$`Task Starting Time`), '%Y-%m-%d')
+    
+    shiftList <- shiftBlocks(day, shifts)
+    
+    shiftedTable <- taskTimes
+    
+    shiftIdx <- 1
+    
+    while(any(shiftedTable$`Task Ending Time` > shiftList[shiftIdx,2])){
+      
+      if(nrow(shiftList) == shiftIdx){
+        
+        day <- as.Date(day) + 1
+        shiftList <- rbind(shiftList, shiftBlocks(day, shifts))
+        
+      }
+      
+      
+      shiftedTable <- shiftedTable %>% mutate(
+        
+        newStart = case_when(
+          
+          `Task Ending Time` < shiftList[shiftIdx, 2] ~ `Task Starting Time`,
+          
+          `Task Ending Time` > shiftList[shiftIdx, 2] &
+            `Task Starting Time` > shiftList[shiftIdx, 2] ~
+            `Task Starting Time` + difftime(shiftList[shiftIdx+1, 1], shiftList[shiftIdx, 2]),
+          
+          `Task Ending Time` > shiftList[shiftIdx, 2] &
+            `Task Starting Time` < shiftList[shiftIdx, 2] ~
+            `Task Starting Time`
+        ),
+        
+        newEnd = case_when(
+          
+          `Task Ending Time` > shiftList[shiftIdx, 2] &
+            `Task Starting Time` < shiftList[shiftIdx, 2] ~ as.POSIXct(shiftList[shiftIdx, 2]),
+          
+          TRUE ~ newStart + 60*`Task Runtime`),
+        
+        split = case_when(
+          
+          `Task Ending Time` > shiftList[shiftIdx, 2] &
+            `Task Starting Time` < shiftList[shiftIdx, 2] ~ TRUE,
+          
+          TRUE ~ FALSE)
+        
+      )
+      
+      shiftedTable <- rbind(shiftedTable,
+                            shiftedTable %>% filter(split == TRUE) %>%
+                              mutate(split = FALSE,
+                                     newEnd = as.POSIXct(shiftList[shiftIdx+1, 1]) + 60*`Task Runtime` - difftime(newEnd, newStart),
+                                     newStart = as.POSIXct(shiftList[shiftIdx+1, 1])
+                              )
+      )
+      
+      shiftedTable <- shiftedTable %>% 
+        mutate(`Task Starting Time` = newStart,
+               `Task Ending Time` = newEnd,
+               `Task Runtime` = as.numeric(difftime(newEnd, newStart, units = 'mins'))) %>%
+        select(-newStart, -newEnd, -split)
+      
+      shiftIdx <- shiftIdx + 1
+      
+    }
+    
+    resultList <- list("Tasks" = shiftedTable, 
+                       "Shifts" = shiftList)
+    
+    return(resultList)
+    
+  }
+  
+  # Refresh startValues
+  shifted <- shiftedTasks(startValues, shift)
+  
+  # Generate data frame for a Job-based timeline
+  jobsView <- data.frame("start" = append(shifted$Tasks$`Task Starting Time`, as.POSIXct(shifted$Shifts[,1])),
+                         "end" = append(shifted$Tasks$`Task Ending Time`, as.POSIXct(shifted$Shifts[,2])),
+                         "content" = append(shifted$Tasks$`Task Name`, rep('Shift',nrow(shifted$Shifts))),
+                         "group" = append(shifted$Tasks$`Job ID`, rep(NA,nrow(shifted$Shifts))),
+                         "type" = c(rep("range",nrow(shifted$Tasks)),rep("background",nrow(shifted$Shifts))))
+  
+  jobsViewGroups <- data.frame(id = unique(shifted$Tasks$`Job ID`), 
+                               content = c(sprintf(paste("Job %s"),seq(1:n_distinct(shifted$Tasks$`Job ID`))))
   )
   
   # Generate data frame for a Machine-based timeline
-  machinesView <- data.frame("start" = startValues$`Task Starting Time`,
-                             "end" = startValues$`Task Ending Time`,
-                             "content" = startValues$`Task Name`,
-                             "group" = startValues$`Machine ID`)
+  machinesView <- data.frame("start" = append(shifted$Tasks$`Task Starting Time`, as.POSIXct(shifted$Shifts[,1])),
+                             "end" = append(shifted$Tasks$`Task Ending Time`, as.POSIXct(shifted$Shifts[,2])),
+                             "content" = append(shifted$Tasks$`Task Name`, rep('Shift',nrow(shifted$Shifts))),
+                             "group" = append(shifted$Tasks$`Machine ID`, rep(NA,nrow(shifted$Shifts))),
+                             "type" = c(rep("range",nrow(shifted$Tasks)),rep("background",nrow(shifted$Shifts))))
   
-  machinesViewGroups <- data.frame(id = sort(unique(startValues$`Machine ID`)), 
-                                   content = c(sprintf(paste("Machine %s"), sort(unique(startValues$`Machine ID`))))
+  machinesViewGroups <- data.frame(id = sort(unique(shifted$Tasks$`Machine ID`)), 
+                                   content = c(sprintf(paste("Machine %s"), sort(unique(shifted$Tasks$`Machine ID`))))
   )
   
   results <- list("res" = filename, 
